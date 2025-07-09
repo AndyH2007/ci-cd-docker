0b1bbc0fd43251c4cfeca462e32b4ada84182431
andyh00 <yoboijz00@gmail.com>
2025-07-09 18:58:29 -0500

    Merge remote changes into local repo

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,16 +8,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
-=======
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('🚀 CI/CD with Docker & GitHub Actions is working!');
-});
-
-app.listen(port, () => {
-  console.log(`App running at http://localhost:${port}`);
->>>>>>> 4b897444b443579f23081f318acd40a8199d73d8
 });
